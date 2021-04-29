b12930bc202177301779fb2eaf54864459097d55
skjha <infoskjha.com>
2021-04-29 12:36:43 +0530

    first commit

---
+++ src/main/java/com/sudhir/rest/WelcomeRestController.java
@@ -0,0 +1,16 @@
+package com.sudhir.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+    @GetMapping("/welcome")
+	public String getWelcomeMsg() {
+		
+		String msg="Welcome to Rest Api";
+		
+		return msg;
+	}
+	
+}
